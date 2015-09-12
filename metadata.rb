c35ceb453d1175721e5757e4ff5fbc28c5aae165
Justin Alan Ryan <bitmonk@icloud.com>
2015-09-11 18:02:55 -0700

    freshly generated cookbook.

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'plone'
+maintainer       'Justin Alan Ryan'
+maintainer_email 'bitmonk@icloud.com'
+license          'Apache 2.0'
+description      'Installs/Configures plone'
+long_description 'Installs/Configures plone'
+version          '0.1.0'
